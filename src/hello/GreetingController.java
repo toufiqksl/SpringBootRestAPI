bb60f1a38812855af7907a95859cee2cd563088a
Toufiq Mahmud <toufiq@ebytelab.com>
2016-07-15 11:20:53 +0600

    Rest Controller POST Method update

--- src/hello/GreetingController.java
+++ src/hello/GreetingController.java
@@ -1,6 +1,8 @@
 package hello;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +12,13 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private static final long id = 100;
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    @RequestMapping(value="/greeting", method=RequestMethod.GET)
+    public Greeting greetingGet(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(id, String.format(template, name));
+    }
+    
+    @RequestMapping(value="/greeting", method=RequestMethod.POST)
+    public Greeting greetingPost(@RequestBody String name) {
         return new Greeting(id, String.format(template, name));
     }
 }
